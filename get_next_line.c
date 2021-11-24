128ae57c98937f16f4766b2b75b02562e36907c5
jibot <ibotjules@gmail.com>
2021-11-24 19:07:13 +0100

    full OK comment, test sans leaks mais KO

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: jibot <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/14 16:20:05 by jibot             #+#    #+#             */
-/*   Updated: 2021/11/23 16:56:19 by jibot            ###   ########.fr       */
+/*   Updated: 2021/11/24 19:01:01 by jibot            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "get_next_line.h"
@@ -30,7 +30,7 @@ int get_save(char *buffer, char *save)
 	static int	flag;
 
 	i = 0;
-	if (flag != 0)
+	if (!save && flag != 0)
 		return (-1);
 	else if (!save && flag == 0)
 	{
@@ -45,21 +45,20 @@ int get_save(char *buffer, char *save)
 	return (i);
 }
 
-char	*get_full_line(char *save, int fd)
+char	*get_full_line(char *line, int fd)
 {
 	char	buffer[BUFFER_SIZE + 1];
-	//char	*line;
 	int		nbyte;
 
-	while (!is_line(save))
+	while (!is_line(line))
 	{
 		ft_bzero(buffer, BUFFER_SIZE + 1);
 		nbyte = read(fd, buffer, BUFFER_SIZE);
 		if (nbyte == 0)
-			return (save);
-		save = ft_strjoin(save, buffer);
+			return (line);
+		line = ft_strjoin(line, buffer);
 	}
-	return (save);
+	return (line);
 }
 
 char	*get_next_line(int fd)
@@ -70,44 +69,56 @@ char	*get_next_line(int fd)
 	int					i;
 	int					nbyte;
 
-	ft_bzero(buffer, BUFFER_SIZE + 1);
 	i = get_save(buffer, save);
+	ft_bzero(buffer + i, BUFFER_SIZE - i + 1);
 	nbyte = read(fd, buffer + i, BUFFER_SIZE - i);
 	if (i == -1 || !buffer[0] || nbyte < 0 || fd < 0 || fd > OPEN_MAX)
 		return (NULL);
 	else if (is_line(buffer) > 0 || buffer[0] == '\n')
+		line = ft_strdup(buffer);
+	else
+		line = get_full_line(ft_strdup(buffer), fd);
+	free(save);
+	if (is_line(line) && line[ft_strlen(line) - 1] != '\n')
 	{
-		save = ft_strdup(buffer + is_line(buffer) + 1);
+		save = ft_strdup(line + is_line(line) + 1);
+		line[is_line(line) + 1] = '\0';
+	}
+	else
+		save = NULL;
+	return (line);
+	/*else if (is_line(buffer) > 0 || buffer[0] == '\n')
+	{
+		free(save);
+		if (buffer[BUFFER_SIZE - 1] == '\n')
+			save = NULL;
+		else
+			save = ft_strdup(buffer + is_line(buffer) + 1);
 		buffer[is_line(buffer) + 1] = '\0';
 		return (ft_strdup(buffer));
 	}
 	else
 	{
-		save = ft_strdup(buffer);
-		//printf("||%s||--", save);
-		save = get_full_line(save, fd);
-		//printf("||%s||%d\n", save, i);
-		line = ft_strdup(save);
-		if (is_line(save) != 0)
+		line = get_full_line(ft_strdup(buffer), fd);
+		free(save);
+		if (is_line(line) && line[ft_strlen(line) - 1] != '\n')
 		{
-			save = ft_strdup(save + is_line(save) + 1);
+			save = ft_strdup(line + is_line(line) + 1);
 			line[is_line(line) + 1] = '\0';
 		}
 		else
-		{
-			line = ft_strdup(save);
 			save = NULL;
-		}
 		return (line);
-	}
+	}*/
 }
 
 /*int	main(void)
 {
-	//int	fd = open("testdoc.txt", O_RDONLY);
+	int	fd = open("testdoc.txt", O_RDONLY);
 	//int	fd = open("3char.txt", O_RDONLY);
-	//int	fd = open("nl.txt", O_RDONLY);
-	int	fd = open("41_no_nl.txt", O_RDONLY);
+	//int	fd = open("BUFFER_SIZEnl.txt", O_RDONLY);
+	//int	fd = open("41_no_nl.txt", O_RDONLY);
+	//int	fd = open("41_with_nl.txt", O_RDONLY);
 	int i = 0;
 	char	*line;
 
